e954164c0caae41819c21a3ed7949314617235b4
Sypityi Pavlo <psypitiy@teamworkcommerce.com>
2023-04-07 16:29:43 +0300

    Test

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -22,8 +22,8 @@ export async function setupAuth() {
         }
     });
 
-    netlifyIdentity.on("login", () => window.location.reload())
-    netlifyIdentity.on("logout", () => window.location.reload())
+    netlifyIdentity.on("login", (...args) => console.log("login", {args}))
+    netlifyIdentity.on("logout", (...args) => console.log("logout", {args}))
 
     //netlifyIdentity.init();
 }
